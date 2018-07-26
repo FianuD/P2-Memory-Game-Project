8d1240bd8508ad721cfef1d1594deef707c0f487
FianuD <dovlofianu@gmail.com>
2018-07-26 23:06:52 +0000

    Implemented shuffle function to the game

--- js/app.js
+++ js/app.js
@@ -10,15 +10,31 @@ const cardsContainer = document.querySelector(".deck");
 let openCards = [];
 let matchedCards =[];
 
+// Shuffle function from http://stackoverflow.com/a/2450976
+function shuffle(icons) {
+    var currentIndex = icons.length, temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = icons[currentIndex];
+        icons[currentIndex] = icons[randomIndex];
+        icons[randomIndex] = temporaryValue;
+    }
+
+    return icons;
+}
+
 /*
  *Function to initiliaze/start the game
  */
 function init(){
     // Loop through icons array and Create the cards
-    for(let i = 0; i < icons.length; i++){
+    let shuffledCards = shuffle(icons);
+    for(let i = 0; i < shuffledCards.length; i++){
         const card = document.createElement("li");
         card.classList.add("card");
-        card.innerHTML = `<i class="${icons[i]}"></i>`;
+        card.innerHTML = `<i class="${shuffledCards[i]}"></i>`;
         cardsContainer.appendChild(card);
         //call click function
         click(card);
@@ -114,7 +130,6 @@ function rating(){
     }
 }
 
-
 /*
  * Restart the game
  */
@@ -137,37 +152,3 @@ restartButton.addEventListener("click", function(){
 
 // Start/Initialize the game for the first time
 init();
-
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
-
-// Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-
-    return array;
-}
-
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
