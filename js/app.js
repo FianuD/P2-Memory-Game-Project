21b7d2849d82e834651904dab6dfd5619387a237
FianuD <dovlofianu@gmail.com>
2018-08-05 03:07:00 +0000

    Added timer. Added modal with full functionality.

--- js/app.js
+++ js/app.js
@@ -1,14 +1,27 @@
-/*
- * Create a list that holds all of your cards
- */
-
+// list that holds all icons
 const icons = ['far fa-gem', 'far fa-gem', 'far fa-paper-plane', 'far fa-paper-plane', 'fas fa-anchor', 'fas fa-anchor', 'fas fa-bolt', 'fas fa-bolt', 'fas fa-cube', 'fas fa-cube', 'fas fa-leaf', 'fas fa-leaf', 'fas fa-bicycle', 'fas fa-bicycle', 'fas fa-bomb', 'fas fa-bomb'];
 
+// Container for cards on page
 const cardsContainer = document.querySelector(".deck");
 
-//Arrays to store cards that have been opened and matched
+// Arrays to store cards that have been opened and matched
 let openCards = [];
 let matchedCards =[];
+
+// Container for star rating system
+const starsContainer = document.querySelector(".stars");
+starsContainer.innerHTML = `<li><i class="fa fa-star"></i></li>
+<li><i class="fa fa-star"></i></li>
+<li><i class="fa fa-star"></i></li>`;
+
+// Modal on game completion containers
+let modal = document.getElementById("gameOver");
+// Stars list
+let starsList = document.querySelectorAll(".star li");
+// Close icon in modal
+let closeIcon = document.querySelector(".close-icon");
+// Play again icon in modal
+const playAgain = document.querySelector(".play-again");
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(icons) {
@@ -42,7 +55,7 @@ function init(){
 }
 
 /*
- * Click Event
+ * Click Event function
  */
 function click(card){
     //Event for when card is clicked
@@ -61,6 +74,26 @@ function click(card){
             openCards.push(this);
         }
     });
+}
+
+/*
+ * Function Reset Game State
+ */
+function resetGameState(){
+    // Delete all cards
+    cardsContainer.innerHTML = "";
+    // Call init function to create new cards
+    init();
+    // Reset any related variables
+    matchedCards = [];
+    moves = 0;
+    movesContainer.innerHTML = moves;
+    starsContainer.innerHTML = `<li><i class="fa fa-star"></i></li>
+    <li><i class="fa fa-star"></i></li>
+    <li><i class="fa fa-star"></i></li>`;
+    // Reset Timer
+    timer.innerHTML = `0 Mins 0 Secs`;
+    clearInterval(interval);
 }
 
 /*
@@ -89,14 +122,36 @@ function compare(currentCard, previousCard){
     addMove();
 }
 
-/*
- * Check if the game is over
- */
+// Check when game is over
 function gameComplete(){
     if(matchedCards.length === icons.length){
-        alert("GAME OVER!");
+        clearInterval(interval);
+        finalTime = timer.innerHTML;
+        // alert("GAME OVER!");
+        // Show congratulations modal
+        modal.classList.add("show");
+        // Declare star rating variable
+        let starRating = document.querySelector(".stars").innerHTML;
+        // Show moves, rating and time on modal
+        document.getElementById("finalMove").innerHTML = moves;
+        document.getElementById("starRating").innerHTML = starRating;
+        document.getElementById("totalTime").innerHTML = finalTime;
+        // Close icon on modal
+        closeModal();
     }
 }
+
+// Close icon on modal functionality
+closeIcon.addEventListener("click", function(e){
+    modal.classList.remove("show");
+});
+
+// Play Again button in modal functionality
+playAgain.addEventListener("click", function(){
+    resetGameState();
+    modal.classList.remove("show");
+});
+
 
 /*
  * Moves counter
@@ -109,15 +164,38 @@ function addMove(){
     movesContainer.innerHTML = moves;
     // Set rating
     rating();
+    // start timer on first move
+    if (moves === 1){
+        second = 0;
+        minute = 0;
+        hour = 0;
+        startTimer();
+    }
+}
+
+/*
+ * Game timer
+ */
+let second = 0, minute = 0;
+const timer = document.querySelector(".timer");
+let interval;
+function startTimer(){
+    interval = setInterval(function(){
+        timer.innerHTML = `${minute} Mins ${second} Secs`
+        second++;
+        if(second===60){
+            minute++;
+            second = 0;
+        } if (minute === 60){
+            hour++;
+            minute = 0;
+        }
+    }, 1000);
 }
 
 /*
  * Star rating system next to moves counter
  */
-const starsContainer = document.querySelector(".stars");
-starsContainer.innerHTML = `<li><i class="fa fa-star"></i></li>
-<li><i class="fa fa-star"></i></li>
-<li><i class="fa fa-star"></i></li>`;
 function rating(){
     if (moves > 12 && moves < 16){
         starsContainer.innerHTML = `<li><i class="fa fa-star"></i></li>
@@ -135,19 +213,7 @@ function rating(){
  */
 const restartButton = document.querySelector(".restart");
 restartButton.addEventListener("click", function(){
-    // Delete all cards
-    cardsContainer.innerHTML = "";
-
-    // Call init function to create new cards
-    init();
-
-    // Reset any related variables
-    matchedCards = [];
-    moves = 0;
-    movesContainer.innerHTML = moves;
-    starsContainer.innerHTML = `<li><i class="fa fa-star"></i></li>
-    <li><i class="fa fa-star"></i></li>
-    <li><i class="fa fa-star"></i></li>`;
+    resetGameState();
 });
 
 // Start/Initialize the game for the first time
